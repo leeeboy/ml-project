53e29af3d1a6e36eee135696d92ea7c2e97ab45e
leeeboy <leelalochan@gmail.com>
2025-04-04 23:44:33 -0500

    EDA and Model files commit

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-
+from logger import logging
 def error_message_detail(error, error_details:sys):
     _, _,exc_tb = error_details.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
@@ -9,7 +9,7 @@ def error_message_detail(error, error_details:sys):
 
 class CustomException(Exception):
     def __init__(self, error_message, error_detail:sys):
-        super.__init__(error_message)
+        super().__init__(error_message)
         self.error_message = error_message_detail(error_message, error_details=error_detail)
 
     def __str__(self):
